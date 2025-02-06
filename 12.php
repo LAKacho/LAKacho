f5c8f79d8addc99fa91308b7948fd6a141e88b32
LAKacho <90930884+LAKacho@users.noreply.github.com>
2025-02-06 11:05:02 +0300

    Update 12.php

--- 12.php
+++ 12.php
@@ -5,41 +5,49 @@ require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-
-$conn1 = new mysql("localhost","u159215", "","b159215_intern"); //данный куда все будет записываться и мыло 
-if ($conn1-> connect_error) {
-	die("Соеденения нет1" . $conn1->connect_error);
-	}
-	
-$conn2 = new mysql("localhost","u159215", "","1test_system");// откуда все будет браться оценки  
-if ($conn2->connect_error) {
-	die ("Соеденения нет2" . $conn2-> connect_error);
+// Подключение к базе данных 1 (куда будут записываться данные и email)
+$conn1 = new mysqli("localhost", "u159215", "", "b159215_intern");
+if ($conn1->connect_error) {
+    die("Ошибка соединения с базой 1: " . $conn1->connect_error);
 }
+
+// Подключение к базе данных 2 (откуда берутся оценки)
+$conn2 = new mysqli("localhost", "u159215", "", "1test_system");
+if ($conn2->connect_error) {
+    die("Ошибка соединения с базой 2: " . $conn2->connect_error);
+}
+
+// Запрос на получение студентов с оценкой 2 за последнюю неделю
 $sql1 = "SELECT user_login
-		FROM xtvr_sesions24 
-		WHERE lesson_name IN (303, 305) AND lesson_result <= 72 AND time_in >= CURDATE() - INTERVAL 7 DAY"; // Двойки берем за неделю
+        FROM xtvr_sesions24 
+        WHERE lesson_name IN (303, 305) AND lesson_result <= 72 AND time_in >= CURDATE() - INTERVAL 7 DAY";
+
 $result1 = $conn2->query($sql1);
-if ($result->num_rows > 0) {
-	$conn1->query("TRUNCETE TABLE dva"); // удаляем все за прошлую неделю
-	
-	
-	 $mail = new PHPMailer(true);
-	 
-	 try { 
-		$mail->isSMTP();
-		$mail->Host = 'ssl://smtp.yandex.ru';
-		$mail->SMTPAuth = true;
-		$mail->Username = '';
-		$mail->Password = '';
-		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-		$mail->Post = 465; 
-		$mail->setFrom(''.'УЦ АТБ'); 
-		$mail->SUBject= 'Увидомления о низкой оценк';// Данные от емайла 
-		
-		while ($row = $result->fetch_assoc()) {
-			$login = $row['tb_nomer'];
-			
-			$sql2="SELECT email from intern_all WHERE tb_nomer = ?";// Почты берем
+
+if ($result1->num_rows > 0) {
+    // Очищаем таблицу перед новой записью
+    $conn1->query("TRUNCATE TABLE dva");
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // Настройки для отправки почты
+        $mail->isSMTP();
+        $mail->Host = 'ssl://smtp.yandex.ru';
+        $mail->SMTPAuth = true;
+        $mail->Username = ''; // Укажите ваш SMTP логин
+        $mail->Password = ''; // Укажите ваш SMTP пароль
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 465; 
+        $mail->setFrom('your_email@example.com', 'УЦ АТБ');
+        $mail->Subject = 'Уведомление о низкой оценке';
+
+        // Обрабатываем результаты запроса
+        while ($row = $result1->fetch_assoc()) {
+            $login = $row['user_login'];
+
+            // Запрос для получения email из другой базы данных
+            $sql2 = "SELECT email FROM intern_all WHERE tb_nomer = ?";
             $stmt = $conn1->prepare($sql2);
             $stmt->bind_param("i", $login);
             $stmt->execute();
@@ -48,15 +56,17 @@ if ($result->num_rows > 0) {
             $stmt->close();
 
             if ($email) {
-                $insert_sql = "INSERT INTO dva (login, email, grade) VALUES (?, ?, ?)";// записываем в базу 
+                // Записываем информацию в таблицу 'dva'
+                $insert_sql = "INSERT INTO dva (login, email, grade) VALUES (?, ?, ?)";
                 $insert_stmt = $conn1->prepare($insert_sql);
                 $grade = 2;
                 $insert_stmt->bind_param("isi", $login, $email, $grade);
                 $insert_stmt->execute();
                 $insert_stmt->close();
 
-                $mail->addAddress($email); // отправляем уведомления
-                $mail->Body = "Уважаемый сотрудник";// текс придумай 
+                // Отправляем уведомление на email
+                $mail->addAddress($email);
+                $mail->Body = "Уважаемый сотрудник, у вас низкая оценка за последние уроки. Пожалуйста, свяжитесь с вашим руководителем.";
                 $mail->send();
                 $mail->clearAddresses();
             }
@@ -72,4 +82,5 @@ if ($result->num_rows > 0) {
 
 $conn1->close();
 $conn2->close();
+
 ?>
